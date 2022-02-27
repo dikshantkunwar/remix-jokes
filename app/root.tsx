0e4fb4c2d4a0fd0036248e8e60018922d254735f
Dikshant Kunwar <dikshantkunwar@gmail.com>
2022-02-27 00:46:09 -0500

    added style links in jokes and root files

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,28 @@
+import type { LinksFunction } from "remix";
 import { Links, LiveReload, Outlet } from "remix";
+
+import globalStyles from "./styles/global.css";
+import globalMediumStyles from "./styles/global-medium.css";
+import globalLargeStyles from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStyles,
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStyles,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStyles,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
+};
 
 export default function App() {
   return (
